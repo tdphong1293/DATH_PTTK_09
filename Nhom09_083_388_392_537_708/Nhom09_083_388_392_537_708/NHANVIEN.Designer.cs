a0ff04eceb96b742fae2cb86e71fb97453553d7d
Shiphu <psphu2003@gmail.com>
2024-04-02 16:33:43 +0700

    Create NHANVIEN.cs and tab function

---
+++ Nhom09_083_388_392_537_708/Nhom09_083_388_392_537_708/NHANVIEN.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Nhom09_083_388_392_537_708
+{
+    partial class NHANVIEN
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl_NV = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabControl_NV.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl_NV
+            // 
+            this.tabControl_NV.Controls.Add(this.tabPage1);
+            this.tabControl_NV.Controls.Add(this.tabPage2);
+            this.tabControl_NV.Controls.Add(this.tabPage3);
+            this.tabControl_NV.Controls.Add(this.tabPage4);
+            this.tabControl_NV.Location = new System.Drawing.Point(1, 3);
+            this.tabControl_NV.Name = "tabControl_NV";
+            this.tabControl_NV.SelectedIndex = 0;
+            this.tabControl_NV.Size = new System.Drawing.Size(1310, 673);
+            this.tabControl_NV.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1302, 647);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Quản lý phiếu đăng tuyển";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1302, 647);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Xử lý hồ sơ ứng tuyển";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1302, 647);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Thanh toán";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(1302, 647);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Danh sách doanh nghiệp tiềm năng";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // NHANVIEN
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1312, 678);
+            this.Controls.Add(this.tabControl_NV);
+            this.Name = "NHANVIEN";
+            this.Text = "NHANVIEN";
+            this.tabControl_NV.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl_NV;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+    }
+}
+
--- Nhom09_083_388_392_537_708/Nhom09_083_388_392_537_708/NHANVIEN.cs
+++ Nhom09_083_388_392_537_708/Nhom09_083_388_392_537_708/NHANVIEN.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Nhom09_083_388_392_537_708
 {
-    public partial class Form1 : Form
+    public partial class NHANVIEN : Form
     {
-        public Form1()
+        public NHANVIEN()
         {
             InitializeComponent();
         }
