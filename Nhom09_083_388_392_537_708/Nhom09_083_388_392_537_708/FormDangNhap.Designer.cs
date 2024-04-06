ce6b89dcdd9b706825322a6edb114ba81a60a45a
zRush1202 <phutrong.ltv@gmail.com>
2024-04-06 17:57:19 +0700

    add form menu, login

---
+++ Nhom09_083_388_392_537_708/Nhom09_083_388_392_537_708/FormDangNhap.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Nhom09_083_388_392_537_708
+{
+    partial class FormDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(282, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đăng nhập";
+            // 
+            // FormDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1261, 695);
+            this.Controls.Add(this.label1);
+            this.Name = "FormDangNhap";
+            this.Text = "FormDangNhap";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Nhom09_083_388_392_537_708/Nhom09_083_388_392_537_708/MainMenuForm.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace Nhom09_083_388_392_537_708
+{
+    partial class MainMenuForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlMenu = new System.Windows.Forms.Panel();
+            this.pnlHome = new System.Windows.Forms.Panel();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnSignup = new System.Windows.Forms.Button();
+            this.btnDangTuyenDung = new System.Windows.Forms.Button();
+            this.btnXemPDT = new System.Windows.Forms.Button();
+            this.btnDuyetHSDaQuaXL = new System.Windows.Forms.Button();
+            this.btnXLDTD = new System.Windows.Forms.Button();
+            this.btnXLHSUngTuyen = new System.Windows.Forms.Button();
+            this.btnThanhToan = new System.Windows.Forms.Button();
+            this.btnGiaHanHD = new System.Windows.Forms.Button();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.pnlTitle = new System.Windows.Forms.Panel();
+            this.pnlHomeChange = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblHome = new System.Windows.Forms.Label();
+            this.pnlMenu.SuspendLayout();
+            this.pnlHome.SuspendLayout();
+            this.pnlTitle.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlMenu
+            // 
+            this.pnlMenu.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pnlMenu.Controls.Add(this.btnLogout);
+            this.pnlMenu.Controls.Add(this.btnGiaHanHD);
+            this.pnlMenu.Controls.Add(this.btnThanhToan);
+            this.pnlMenu.Controls.Add(this.btnXLHSUngTuyen);
+            this.pnlMenu.Controls.Add(this.btnXLDTD);
+            this.pnlMenu.Controls.Add(this.btnDuyetHSDaQuaXL);
+            this.pnlMenu.Controls.Add(this.btnXemPDT);
+            this.pnlMenu.Controls.Add(this.btnDangTuyenDung);
+            this.pnlMenu.Controls.Add(this.btnSignup);
+            this.pnlMenu.Controls.Add(this.btnLogin);
+            this.pnlMenu.Controls.Add(this.pnlHome);
+            this.pnlMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlMenu.Location = new System.Drawing.Point(0, 0);
+            this.pnlMenu.Name = "pnlMenu";
+            this.pnlMenu.Size = new System.Drawing.Size(226, 1033);
+            this.pnlMenu.TabIndex = 0;
+            // 
+            // pnlHome
+            // 
+            this.pnlHome.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pnlHome.Controls.Add(this.lblHome);
+            this.pnlHome.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHome.Location = new System.Drawing.Point(0, 0);
+            this.pnlHome.Name = "pnlHome";
+            this.pnlHome.Size = new System.Drawing.Size(226, 115);
+            this.pnlHome.TabIndex = 0;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnLogin.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Location = new System.Drawing.Point(0, 115);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(226, 90);
+            this.btnLogin.TabIndex = 1;
+            this.btnLogin.Text = "Đăng nhập";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnSignup
+            // 
+            this.btnSignup.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnSignup.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSignup.FlatAppearance.BorderSize = 0;
+            this.btnSignup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSignup.Location = new System.Drawing.Point(0, 205);
+            this.btnSignup.Name = "btnSignup";
+            this.btnSignup.Size = new System.Drawing.Size(226, 90);
+            this.btnSignup.TabIndex = 2;
+            this.btnSignup.Text = "Đăng ký thành viên";
+            this.btnSignup.UseVisualStyleBackColor = false;
+            // 
+            // btnDangTuyenDung
+            // 
+            this.btnDangTuyenDung.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnDangTuyenDung.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDangTuyenDung.FlatAppearance.BorderSize = 0;
+            this.btnDangTuyenDung.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDangTuyenDung.Location = new System.Drawing.Point(0, 295);
+            this.btnDangTuyenDung.Name = "btnDangTuyenDung";
+            this.btnDangTuyenDung.Size = new System.Drawing.Size(226, 90);
+            this.btnDangTuyenDung.TabIndex = 3;
+            this.btnDangTuyenDung.Text = "Đăng tuyển dụng";
+            this.btnDangTuyenDung.UseVisualStyleBackColor = false;
+            // 
+            // btnXemPDT
+            // 
+            this.btnXemPDT.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnXemPDT.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnXemPDT.FlatAppearance.BorderSize = 0;
+            this.btnXemPDT.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnXemPDT.Location = new System.Drawing.Point(0, 385);
+            this.btnXemPDT.Name = "btnXemPDT";
+            this.btnXemPDT.Size = new System.Drawing.Size(226, 90);
+            this.btnXemPDT.TabIndex = 4;
+            this.btnXemPDT.Text = "Xem phiếu đăng tuyển";
+            this.btnXemPDT.UseVisualStyleBackColor = false;
+            // 
+            // btnDuyetHSDaQuaXL
+            // 
+            this.btnDuyetHSDaQuaXL.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnDuyetHSDaQuaXL.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDuyetHSDaQuaXL.FlatAppearance.BorderSize = 0;
+            this.btnDuyetHSDaQuaXL.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDuyetHSDaQuaXL.Location = new System.Drawing.Point(0, 475);
+            this.btnDuyetHSDaQuaXL.Name = "btnDuyetHSDaQuaXL";
+            this.btnDuyetHSDaQuaXL.Size = new System.Drawing.Size(226, 90);
+            this.btnDuyetHSDaQuaXL.TabIndex = 5;
+            this.btnDuyetHSDaQuaXL.Text = "Duyệt hồ sơ";
+            this.btnDuyetHSDaQuaXL.UseVisualStyleBackColor = false;
+            // 
+            // btnXLDTD
+            // 
+            this.btnXLDTD.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnXLDTD.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnXLDTD.FlatAppearance.BorderSize = 0;
+            this.btnXLDTD.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnXLDTD.Location = new System.Drawing.Point(0, 565);
+            this.btnXLDTD.Name = "btnXLDTD";
+            this.btnXLDTD.Size = new System.Drawing.Size(226, 90);
+            this.btnXLDTD.TabIndex = 6;
+            this.btnXLDTD.Text = "Xử lý đăng tuyển dụng";
+            this.btnXLDTD.UseVisualStyleBackColor = false;
+            // 
+            // btnXLHSUngTuyen
+            // 
+            this.btnXLHSUngTuyen.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnXLHSUngTuyen.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnXLHSUngTuyen.FlatAppearance.BorderSize = 0;
+            this.btnXLHSUngTuyen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnXLHSUngTuyen.Location = new System.Drawing.Point(0, 655);
+            this.btnXLHSUngTuyen.Name = "btnXLHSUngTuyen";
+            this.btnXLHSUngTuyen.Size = new System.Drawing.Size(226, 90);
+            this.btnXLHSUngTuyen.TabIndex = 7;
+            this.btnXLHSUngTuyen.Text = "Xử lý hồ sơ ứng tuyển";
+            this.btnXLHSUngTuyen.UseVisualStyleBackColor = false;
+            // 
+            // btnThanhToan
+            // 
+            this.btnThanhToan.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnThanhToan.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnThanhToan.FlatAppearance.BorderSize = 0;
+            this.btnThanhToan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnThanhToan.Location = new System.Drawing.Point(0, 745);
+            this.btnThanhToan.Name = "btnThanhToan";
+            this.btnThanhToan.Size = new System.Drawing.Size(226, 90);
+            this.btnThanhToan.TabIndex = 8;
+            this.btnThanhToan.Text = "Thanh toán";
+            this.btnThanhToan.UseVisualStyleBackColor = false;
+            // 
+            // btnGiaHanHD
+            // 
+            this.btnGiaHanHD.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnGiaHanHD.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnGiaHanHD.FlatAppearance.BorderSize = 0;
+            this.btnGiaHanHD.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGiaHanHD.Location = new System.Drawing.Point(0, 835);
+            this.btnGiaHanHD.Name = "btnGiaHanHD";
+            this.btnGiaHanHD.Size = new System.Drawing.Size(226, 90);
+            this.btnGiaHanHD.TabIndex = 9;
+            this.btnGiaHanHD.Text = "Gia hạn hợp đồng";
+            this.btnGiaHanHD.UseVisualStyleBackColor = false;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnLogout.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnLogout.FlatAppearance.BorderSize = 0;
+            this.btnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogout.Location = new System.Drawing.Point(0, 925);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(226, 90);
+            this.btnLogout.TabIndex = 10;
+            this.btnLogout.Text = "Đăng xuất";
+            this.btnLogout.UseVisualStyleBackColor = false;
+            // 
+            // pnlTitle
+            // 
+            this.pnlTitle.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.pnlTitle.Controls.Add(this.lblTitle);
+            this.pnlTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTitle.Location = new System.Drawing.Point(226, 0);
+            this.pnlTitle.Name = "pnlTitle";
+            this.pnlTitle.Size = new System.Drawing.Size(1676, 115);
+            this.pnlTitle.TabIndex = 1;
+            // 
+            // pnlHomeChange
+            // 
+            this.pnlHomeChange.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.pnlHomeChange.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlHomeChange.Location = new System.Drawing.Point(226, 115);
+            this.pnlHomeChange.Name = "pnlHomeChange";
+            this.pnlHomeChange.Size = new System.Drawing.Size(1676, 918);
+            this.pnlHomeChange.TabIndex = 2;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(438, 43);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(346, 31);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "HỆ THỐNG TUYỂN DỤNG";
+            // 
+            // lblHome
+            // 
+            this.lblHome.AutoSize = true;
+            this.lblHome.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHome.Location = new System.Drawing.Point(62, 43);
+            this.lblHome.Name = "lblHome";
+            this.lblHome.Size = new System.Drawing.Size(97, 26);
+            this.lblHome.TabIndex = 0;
+            this.lblHome.Text = "NHÓM 9";
+            // 
+            // MainMenuForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1902, 1033);
+            this.Controls.Add(this.pnlHomeChange);
+            this.Controls.Add(this.pnlTitle);
+            this.Controls.Add(this.pnlMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "MainMenuForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainMenuForm";
+            this.pnlMenu.ResumeLayout(false);
+            this.pnlHome.ResumeLayout(false);
+            this.pnlHome.PerformLayout();
+            this.pnlTitle.ResumeLayout(false);
+            this.pnlTitle.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlMenu;
+        private System.Windows.Forms.Panel pnlHome;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Button btnGiaHanHD;
+        private System.Windows.Forms.Button btnThanhToan;
+        private System.Windows.Forms.Button btnXLHSUngTuyen;
+        private System.Windows.Forms.Button btnXLDTD;
+        private System.Windows.Forms.Button btnDuyetHSDaQuaXL;
+        private System.Windows.Forms.Button btnXemPDT;
+        private System.Windows.Forms.Button btnDangTuyenDung;
+        private System.Windows.Forms.Button btnSignup;
+        private System.Windows.Forms.Panel pnlTitle;
+        private System.Windows.Forms.Panel pnlHomeChange;
+        private System.Windows.Forms.Label lblHome;
+        private System.Windows.Forms.Label lblTitle;
+    }
+}
