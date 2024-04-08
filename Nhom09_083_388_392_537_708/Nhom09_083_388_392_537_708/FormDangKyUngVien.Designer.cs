2df7987e3f258e3767a62b95fa7c6d0e6048f093
tdphong1293 <ducphongtang1293@gmail.com>
2024-04-08 15:53:32 +0700

    Form Dang Ky Ung Vien va Doanh Nghiep chua xong

---
+++ Nhom09_083_388_392_537_708/Nhom09_083_388_392_537_708/FormDangKyUngVien.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Nhom09_083_388_392_537_708
+{
+    partial class FormDangKyUngVien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel_FormDangKyUngVien = new System.Windows.Forms.Panel();
+            this.lb_birth = new System.Windows.Forms.Label();
+            this.txt_email = new System.Windows.Forms.TextBox();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.txt_repassword = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.btn_DangKyUngVien = new System.Windows.Forms.Button();
+            this.lb_email = new System.Windows.Forms.Label();
+            this.lb_name = new System.Windows.Forms.Label();
+            this.lb_repassword = new System.Windows.Forms.Label();
+            this.lb_password = new System.Windows.Forms.Label();
+            this.lb_username = new System.Windows.Forms.Label();
+            this.lb_FormDangKyUngVien = new System.Windows.Forms.Label();
+            this.dtp_birth = new System.Windows.Forms.DateTimePicker();
+            this.panel_FormDangKyUngVien.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel_FormDangKyUngVien
+            // 
+            this.panel_FormDangKyUngVien.Controls.Add(this.dtp_birth);
+            this.panel_FormDangKyUngVien.Controls.Add(this.lb_birth);
+            this.panel_FormDangKyUngVien.Controls.Add(this.txt_email);
+            this.panel_FormDangKyUngVien.Controls.Add(this.txt_name);
+            this.panel_FormDangKyUngVien.Controls.Add(this.txt_repassword);
+            this.panel_FormDangKyUngVien.Controls.Add(this.txt_password);
+            this.panel_FormDangKyUngVien.Controls.Add(this.txt_username);
+            this.panel_FormDangKyUngVien.Controls.Add(this.btn_DangKyUngVien);
+            this.panel_FormDangKyUngVien.Controls.Add(this.lb_email);
+            this.panel_FormDangKyUngVien.Controls.Add(this.lb_name);
+            this.panel_FormDangKyUngVien.Controls.Add(this.lb_repassword);
+            this.panel_FormDangKyUngVien.Controls.Add(this.lb_password);
+            this.panel_FormDangKyUngVien.Controls.Add(this.lb_username);
+            this.panel_FormDangKyUngVien.Controls.Add(this.lb_FormDangKyUngVien);
+            this.panel_FormDangKyUngVien.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_FormDangKyUngVien.Location = new System.Drawing.Point(0, 0);
+            this.panel_FormDangKyUngVien.Name = "panel_FormDangKyUngVien";
+            this.panel_FormDangKyUngVien.Size = new System.Drawing.Size(1240, 707);
+            this.panel_FormDangKyUngVien.TabIndex = 17;
+            // 
+            // lb_birth
+            // 
+            this.lb_birth.AutoSize = true;
+            this.lb_birth.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lb_birth.Location = new System.Drawing.Point(282, 421);
+            this.lb_birth.Name = "lb_birth";
+            this.lb_birth.Size = new System.Drawing.Size(76, 20);
+            this.lb_birth.TabIndex = 29;
+            this.lb_birth.Text = "Ngày Sinh";
+            // 
+            // txt_email
+            // 
+            this.txt_email.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txt_email.Location = new System.Drawing.Point(453, 370);
+            this.txt_email.Name = "txt_email";
+            this.txt_email.Size = new System.Drawing.Size(473, 27);
+            this.txt_email.TabIndex = 28;
+            // 
+            // txt_name
+            // 
+            this.txt_name.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txt_name.Location = new System.Drawing.Point(453, 321);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(473, 27);
+            this.txt_name.TabIndex = 27;
+            // 
+            // txt_repassword
+            // 
+            this.txt_repassword.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txt_repassword.Location = new System.Drawing.Point(453, 269);
+            this.txt_repassword.Name = "txt_repassword";
+            this.txt_repassword.Size = new System.Drawing.Size(473, 27);
+            this.txt_repassword.TabIndex = 26;
+            // 
+            // txt_password
+            // 
+            this.txt_password.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txt_password.Location = new System.Drawing.Point(453, 221);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(473, 27);
+            this.txt_password.TabIndex = 25;
+            // 
+            // txt_username
+            // 
+            this.txt_username.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txt_username.Location = new System.Drawing.Point(453, 176);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(473, 27);
+            this.txt_username.TabIndex = 24;
+            // 
+            // btn_DangKyUngVien
+            // 
+            this.btn_DangKyUngVien.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_DangKyUngVien.Location = new System.Drawing.Point(553, 532);
+            this.btn_DangKyUngVien.Name = "btn_DangKyUngVien";
+            this.btn_DangKyUngVien.Size = new System.Drawing.Size(150, 50);
+            this.btn_DangKyUngVien.TabIndex = 23;
+            this.btn_DangKyUngVien.Text = "Đăng ký";
+            this.btn_DangKyUngVien.UseVisualStyleBackColor = true;
+            // 
+            // lb_email
+            // 
+            this.lb_email.AutoSize = true;
+            this.lb_email.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lb_email.Location = new System.Drawing.Point(282, 370);
+            this.lb_email.Name = "lb_email";
+            this.lb_email.Size = new System.Drawing.Size(46, 20);
+            this.lb_email.TabIndex = 22;
+            this.lb_email.Text = "Email";
+            // 
+            // lb_name
+            // 
+            this.lb_name.AutoSize = true;
+            this.lb_name.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lb_name.Location = new System.Drawing.Point(282, 321);
+            this.lb_name.Name = "lb_name";
+            this.lb_name.Size = new System.Drawing.Size(93, 20);
+            this.lb_name.TabIndex = 21;
+            this.lb_name.Text = "Tên ứng viên";
+            // 
+            // lb_repassword
+            // 
+            this.lb_repassword.AutoSize = true;
+            this.lb_repassword.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lb_repassword.Location = new System.Drawing.Point(282, 269);
+            this.lb_repassword.Name = "lb_repassword";
+            this.lb_repassword.Size = new System.Drawing.Size(130, 20);
+            this.lb_repassword.TabIndex = 20;
+            this.lb_repassword.Text = "Nhập lại mật khẩu";
+            // 
+            // lb_password
+            // 
+            this.lb_password.AutoSize = true;
+            this.lb_password.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lb_password.Location = new System.Drawing.Point(282, 221);
+            this.lb_password.Name = "lb_password";
+            this.lb_password.Size = new System.Drawing.Size(70, 20);
+            this.lb_password.TabIndex = 19;
+            this.lb_password.Text = "Mật khẩu";
+            // 
+            // lb_username
+            // 
+            this.lb_username.AutoSize = true;
+            this.lb_username.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lb_username.Location = new System.Drawing.Point(282, 176);
+            this.lb_username.Name = "lb_username";
+            this.lb_username.Size = new System.Drawing.Size(107, 20);
+            this.lb_username.TabIndex = 18;
+            this.lb_username.Text = "Tên đăng nhập";
+            // 
+            // lb_FormDangKyUngVien
+            // 
+            this.lb_FormDangKyUngVien.AutoSize = true;
+            this.lb_FormDangKyUngVien.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_FormDangKyUngVien.Location = new System.Drawing.Point(503, 57);
+            this.lb_FormDangKyUngVien.Name = "lb_FormDangKyUngVien";
+            this.lb_FormDangKyUngVien.Size = new System.Drawing.Size(250, 30);
+            this.lb_FormDangKyUngVien.TabIndex = 17;
+            this.lb_FormDangKyUngVien.Text = "Form Đăng Ký Ứng Viên";
+            // 
+            // dtp_birth
+            // 
+            this.dtp_birth.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.dtp_birth.Location = new System.Drawing.Point(726, 421);
+            this.dtp_birth.Name = "dtp_birth";
+            this.dtp_birth.Size = new System.Drawing.Size(200, 27);
+            this.dtp_birth.TabIndex = 31;
+            // 
+            // FormDangKyUngVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1240, 707);
+            this.Controls.Add(this.panel_FormDangKyUngVien);
+            this.Name = "FormDangKyUngVien";
+            this.Text = "FormDangKyUngVien";
+            this.panel_FormDangKyUngVien.ResumeLayout(false);
+            this.panel_FormDangKyUngVien.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_FormDangKyUngVien;
+        private System.Windows.Forms.Label lb_birth;
+        private System.Windows.Forms.TextBox txt_email;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.TextBox txt_repassword;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.Button btn_DangKyUngVien;
+        private System.Windows.Forms.Label lb_email;
+        private System.Windows.Forms.Label lb_name;
+        private System.Windows.Forms.Label lb_repassword;
+        private System.Windows.Forms.Label lb_password;
+        private System.Windows.Forms.Label lb_username;
+        private System.Windows.Forms.Label lb_FormDangKyUngVien;
+        private System.Windows.Forms.DateTimePicker dtp_birth;
+    }
+}
