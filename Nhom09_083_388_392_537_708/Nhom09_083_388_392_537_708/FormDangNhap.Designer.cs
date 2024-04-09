601a625da2670b79eec3f3941fea040591eb0da2
Shiphuu <psphu2103@gmail.com>
2024-04-09 20:18:15 +0700

    Fix size of form DangNhap

--- Nhom09_083_388_392_537_708/Nhom09_083_388_392_537_708/FormDangNhap.Designer.cs
+++ Nhom09_083_388_392_537_708/Nhom09_083_388_392_537_708/FormDangNhap.Designer.cs
@@ -44,9 +44,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(439, 56);
+            this.label2.Location = new System.Drawing.Point(600, 114);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(345, 31);
+            this.label2.Size = new System.Drawing.Size(283, 26);
             this.label2.TabIndex = 5;
             this.label2.Text = "ĐĂNG NHẬP HỆ THỐNG";
             // 
@@ -54,36 +55,40 @@
             // 
             this.lblUsername.AutoSize = true;
             this.lblUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUsername.Location = new System.Drawing.Point(19, 92);
+            this.lblUsername.Location = new System.Drawing.Point(15, 74);
+            this.lblUsername.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(193, 26);
+            this.lblUsername.Size = new System.Drawing.Size(159, 22);
             this.lblUsername.TabIndex = 4;
             this.lblUsername.Text = "TÊN ĐĂNG NHẬP";
             // 
             // tbxUsername
             // 
             this.tbxUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbxUsername.Location = new System.Drawing.Point(240, 89);
+            this.tbxUsername.Location = new System.Drawing.Point(192, 71);
+            this.tbxUsername.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbxUsername.Name = "tbxUsername";
-            this.tbxUsername.Size = new System.Drawing.Size(390, 32);
+            this.tbxUsername.Size = new System.Drawing.Size(313, 27);
             this.tbxUsername.TabIndex = 6;
             // 
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
             this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPassword.Location = new System.Drawing.Point(19, 168);
+            this.lblPassword.Location = new System.Drawing.Point(15, 134);
+            this.lblPassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(125, 26);
+            this.lblPassword.Size = new System.Drawing.Size(103, 22);
             this.lblPassword.TabIndex = 5;
             this.lblPassword.Text = "MẬT KHẨU";
             // 
             // tbxPassword
             // 
             this.tbxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbxPassword.Location = new System.Drawing.Point(240, 165);
+            this.tbxPassword.Location = new System.Drawing.Point(192, 132);
+            this.tbxPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbxPassword.Name = "tbxPassword";
-            this.tbxPassword.Size = new System.Drawing.Size(390, 32);
+            this.tbxPassword.Size = new System.Drawing.Size(313, 27);
             this.tbxPassword.TabIndex = 7;
             // 
             // btnLogin
@@ -92,9 +97,10 @@
             this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLogin.ForeColor = System.Drawing.Color.White;
-            this.btnLogin.Location = new System.Drawing.Point(203, 287);
+            this.btnLogin.Location = new System.Drawing.Point(162, 230);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(270, 65);
+            this.btnLogin.Size = new System.Drawing.Size(216, 52);
             this.btnLogin.TabIndex = 8;
             this.btnLogin.Text = "ĐĂNG NHẬP";
             this.btnLogin.UseVisualStyleBackColor = false;
@@ -108,9 +114,11 @@
             this.groupBox1.Controls.Add(this.lblUsername);
             this.groupBox1.Controls.Add(this.tbxUsername);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(435, 172);
+            this.groupBox1.Location = new System.Drawing.Point(487, 202);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(650, 358);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(520, 286);
             this.groupBox1.TabIndex = 10;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Thông tin đăng nhập";
@@ -120,23 +128,25 @@
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.pictureBox1.Image = global::Nhom09_083_388_392_537_708.Properties.Resources.login;
-            this.pictureBox1.Location = new System.Drawing.Point(43, 182);
+            this.pictureBox1.Location = new System.Drawing.Point(173, 210);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(368, 348);
+            this.pictureBox1.Size = new System.Drawing.Size(294, 278);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 9;
             this.pictureBox1.TabStop = false;
             // 
             // FormDangNhap
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoSize = true;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1264, 727);
+            this.ClientSize = new System.Drawing.Size(1240, 707);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormDangNhap";
             this.Text = "FormDangNhap";
             this.groupBox1.ResumeLayout(false);
--- Nhom09_083_388_392_537_708/Nhom09_083_388_392_537_708/MainMenuForm.cs
+++ Nhom09_083_388_392_537_708/Nhom09_083_388_392_537_708/MainMenuForm.cs
@@ -204,5 +204,10 @@ namespace Nhom09_083_388_392_537_708
         {
 
         }
+
+        private void btnSignup_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
